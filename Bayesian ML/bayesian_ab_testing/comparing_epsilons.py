a30937ea46c98a28aea790c2135028e77c01e3a0
hangwl <hangwei54e@hotmail.com>
2023-02-23 22:30:05 +0800

    c

---
+++ Bayesian ML/bayesian_ab_testing/comparing_epsilons.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# OUTLINE:
+# 1. define BanditArm class
+# 2. define function that runs experiment over multiple trials
+# 3. print and plot results
+
+# the BanditArm class has two functions
+# 1. pull - which returns the reward m which follows a gaussian distribution
+# 2. update - update the trial counter and m_estimate (sample mean of reward) for the respective bandit explored/exploited
+
+class BanditArm: 
+    def __init__(self, m):
+        self.m = m
+        self.m_estimate = 0
+        self.N = 0
+
+    def pull(self):
+        return np.random.randn() + self.m # in this example, we are drawing a sample from a gaussian distribution with mean m and variance 1 rather than a bernoulli
+
+    def update(self, x):
+        self.N += 1
+        self.m_estimate = (1 - 1.0/self.N)*self.m_estimate + 1.0/self.N*x # the way to calculate sample mean remains unchanged
+
+# in the epsilon-greedy approach, our experiment will be iterated over 3 bandits with their individual respective means m
+# epsilon is defined as the probability that a random bandit will be explored, else we exploit the bandit with the highest estimated m_estimate
+# over each exploration/exploitation, we will update the respective m_estimate
+
+def run_experiment(m1, m2, m3, eps, N):
+    bandits = [BanditArm(m1), BanditArm(m2), BanditArm(m3)]
+
+    # count number of suboptimal choices
+    means = np.array([m1,m2,m3])
+    true_best = np.argmax(means)
+    count_suboptimal = 0
+
+    data = np.empty(N)
+
+    for i in range(N):
+        p = np.random.random()
+        if p < eps:
+            j = np.random.choice(len(bandits))
+        else:
+            j = np.argmax([b.m_estimate for b in bandits])
+        x = bandits[j].pull()
+        bandits[j].update(x)
+
+        if j != true_best:
+            count_suboptimal += 1
+
+        # for the plot
+        data[i] = x
+    cumulative_average = np.cumsum(data) / (np.arange(N) + 1) # np.cumsum() returns an array of consecutive cumulative reward averages i.e. np.cumsum([1,2,3] returns array([1,3,6])) 
+
+    # plot moving average ctr
+    plt.plot(cumulative_average)
+    plt.plot(np.ones(N)*m1)
+    plt.plot(np.ones(N)*m2)
+    plt.plot(np.ones(N)*m3)
+    plt.xscale('log') # these algorithms converge quickly, and using a log scale is useful in this case
+    plt.show()
+
+    for b in bandits:
+        print(b.m_estimate)
+
+    print("percent suboptimal for epsilon = %s:" % eps, float(count_suboptimal) / N)
+
+    return cumulative_average
+
+# in this exercise, we run our experiments over different epsilon values
+# when we compare all 3 plots together, we notice:
+# higher epsilon means that the algorithm converges more quickly to the reward of the optimal bandit;
+# when we compare the long run cumulative reward averages, we notice that for higher epsilon values, the cumulative reward is worse and worse
+# this implies that there is a trade-off between fast convergence and higher eventual reward
+
+if __name__ == "__main__":
+    m1, m2, m3 = 1.5, 2.5, 3.5
+    c_1 = run_experiment(m1, m2, m3, 0.1, 100000)
+    c_05 = run_experiment(m1, m2, m3, 0.05, 100000)
+    c_01 = run_experiment(m1, m2, m3, 0.01, 100000)
+
+    # log scale plot
+    plt.plot(c_1, label='eps = 0.1')
+    plt.plot(c_05, label='eps = 0.05')
+    plt.plot(c_01, label='eps = 0.01')
+    plt.legend()
+    plt.xscale('log')
+    plt.show()
+
+    # linear plot
+    plt.plot(c_1, label='eps = 0.1')
+    plt.plot(c_05, label='eps = 0.05')
+    plt.plot(c_01, label='eps = 0.01')
+    plt.legend()
+    plt.show()
+
---
+++ Bayesian ML/bayesian_ab_testing/epsilon_greedy_starter.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+NUM_TRIALS = 10000
+EPS = 0.1 # Epsilon: Exploration Factor
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75] # Actual win rates of bandits (unknown in reality)
+
+class Bandit:
+    def __init__(self, p):
+        self.p = p # p represents the true win rate for this bandit
+        self.p_estimate = 0 
+        self.N = 0 # number of samples collected
+
+    def pull(self): # Pulling lever of 'slot machine'
+        return np.random.random() < self.p # Return True(1) if win / False(0) if lose
+
+    def update(self, x):
+        self.N = self.N + 1
+        self.p_estimate = ((self.N - 1)*self.p_estimate + x) / self.N # Iteratively updating sample mean (p_estimate)
+
+def experiment():
+    bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+
+    rewards = np.zeros(NUM_TRIALS)
+    num_times_explored = 0
+    num_times_exploited = 0
+    num_optimal = 0
+    optimal_j = np.argmax(([b.p for b in bandits])) # np.argmax() returns the 'indices' if the maximum values along an axis
+    print("optimal j:", optimal_j)
+
+    for i in range(NUM_TRIALS):
+
+        # use epsilon-greedy to select the next bandit
+        if np.random.random() < EPS:
+            num_times_explored += 1
+            j = np.random.randint(len(bandits))
+        else:
+            num_times_exploited += 1
+            j = np.argmax([b.p_estimate for b in bandits])
+        
+        if j == optimal_j:
+            num_optimal += 1
+
+        # pull the arm for the bandit with the largest sample
+        x = bandits[j].pull()
+
+        # update rewards log
+        rewards[i] = x
+
+        # update the distribution for the bandit whose arm we just pulled
+        bandits[j].update(x)
+
+
+    # print mean estimates for each bandit
+    for b in bandits:
+        print("mean estimate:", b.p_estimate)
+
+    # print total reward
+    print("total reward earned:", rewards.sum())
+    print("overall win rate:", rewards.sum() / NUM_TRIALS)
+    print("num_times_explored:", num_times_explored)
+    print("num_times_exploited:", num_times_exploited)
+    print("num times selected optimal bandit:", num_optimal)
+
+    # plot the results
+    cumulative_rewards = np.cumsum(rewards)
+    win_rates = cumulative_rewards / (np.arange(NUM_TRIALS) + 1)
+    plt.plot(win_rates)
+    plt.plot(np.ones(NUM_TRIALS)*np.max(BANDIT_PROBABILITIES))
+    plt.show()
+
+if __name__ == "__main__":
+    experiment()
